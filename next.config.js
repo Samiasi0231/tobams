1868ab679eb588a3fe73a29fb78585f75ee27abb
samiasi0231 <samuelasime@gmail.com>
2025-02-02 02:05:04 +0100

    Fix dependency

--- next.config.js
+++
@@ -1,9 +0,0 @@
-// next.config.js
-const withPWA = require('next-pwa');
-
-module.exports = withPWA({
-  pwa: {
-    dest: 'public', // Destination for service worker and manifest files
-    disable: process.env.NODE_ENV === 'development', // Disable service worker in development
-  },
-});
